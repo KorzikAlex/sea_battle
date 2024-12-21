1697fbdf407228c3c6ced53da063786968cf08d0
KorzikAlex <lumic.alex@gmail.com>
2024-12-22 01:38:00 +0300

    restuctorization again delete exceptions *.cpp writing ser/deser of file

---
+++ scripts/Linux/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if command -v ninja &> /dev/null; then
+    echo "Using Ninja for build..."
+    cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE --no-warn-unused-cli -S.. -B../build -G "Ninja"
+    ninja -C ../build
+else
+    echo "Ninja not found, using Make for build..."
+    cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE --no-warn-unused-cli -S.. -B../build -G "Unix Makefiles"
+    make --directory="../build/"
+fi
---
+++ scripts/Linux/docs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ..
+doxygen Doxyfile
+xdg-open docs/html/index.html
